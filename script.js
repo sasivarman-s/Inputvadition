c60963c2e24d4e9d4b3b22110f39a47d3e457478
sasivarman-s <sureshraman2680@gmail.com>
2026-07-24 15:14:28 +0530

    updated push

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let email = document.getElementById("email")
-let password = document.getElementById("pass")
-let info1 = document.getElementById("info1")
-let info2 = document.getElementById("info2")
-let btn = document.getElementById("btn")
-btn.addEventListener("click", (e)=>{
+var email = document.getElementById("email")
+var password = document.getElementById("pass")
+var info1 = document.getElementById("info1")
+var info2 = document.getElementById("info2")
+var btn = document.getElementById("btn")
+btn.addEventListener("click", (e) => {
     e.preventDefault()
     console.log(email.value);
     console.log(password.value);
-    validation(email,password)
-    
+    validation(email.value, password.value)
+
 })
 
-function validation(email,password){
-     var Vemail = email.value
-     var Vpassword = password.value
-    
+function validation(email, password) {
+    const emailregex = /^\S+@\S+\.\S+$/
+    const passwordregex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+
+    if (email.match(emailregex)) {
+        info1.innerHTML = "Email validation successful"
+        info1.style.color = "darkgreen"
+    } else {
+        info1.innerHTML = "Invalid Email format"
+    }
+
+    if (password.match(passwordregex)) {
+        info2.innerHTML = "Password validation successful"
+        info2.style.color = "darkgreen"
+    } else {
+        info2.innerHTML = "Invalid password format"
+    }
 }
